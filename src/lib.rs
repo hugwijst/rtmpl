9dc51e2d27d842ae34c0a62d506e442f2f338bf3
Hugo van der Wijst <hugo@wij.st>
2014-11-16 23:21:44 +0100

    First commit.
    Plugin infrastructure in place. First and very rough version of an
    RTTI system is working.

---
+++ src/lib.rs
@@ -0,0 +1,225 @@
+#![feature(macro_rules)]
+#![feature(trace_macros)]
+
+#![feature(plugin_registrar)]
+
+extern crate syntax;
+extern crate rustc;
+
+use rustc::plugin::Registry;
+use syntax::ast::{CookedStr, Expr, Ident, Item, ItemStruct, LitInt, LitStr, MetaItem, MetaWord, MutImmutable, Plus, UnsuffixedIntLit};
+use syntax::ast::{NamedField, UnnamedField};
+use syntax::ast::{TyPath, PathSegment};
+use syntax::codemap::{Span, Spanned};
+use syntax::ext::base::{Decorator, ExtCtxt};
+use syntax::ext::build::AstBuilder;
+use syntax::ext::deriving::generic::{combine_substructure, MethodDef, Substructure, TraitDef};
+use syntax::ext::deriving::generic::ty::{borrowed, borrowed_explicit_self, borrowed_self, nil_ty, LifetimeBounds, Literal, Path, Self};
+use syntax::parse::token;
+use syntax::parse::token::InternedString;
+use syntax::ptr::P;
+
+#[deriving(Show,PartialEq)]
+pub enum AttrType {
+    StringType,
+    IntType,
+    UintType,
+}
+
+pub trait Model {
+    fn __get_type(attr: &str, _ignored: Option<Self>) -> Option<AttrType>;
+    fn __get_string(&self, attr: &str) -> &str;
+    fn __get_int(&self, attr: &str) -> int;
+    fn __get_uint(&self, attr: &str) -> uint;
+}
+
+fn path_to_attr_type(path: &syntax::ast::Path) -> Option<AttrType> {
+    let std_ = token::intern("std");
+    let string_ = token::intern("string");
+    let string__ = token::intern("String");
+
+    let int_  = token::intern("int");
+    let i8_   = token::intern("i8");
+    let i16_  = token::intern("i16");
+    let i32_  = token::intern("i32");
+    let uint_ = token::intern("uint");
+    let u8_   = token::intern("u8");
+    let u16_  = token::intern("u16");
+    let u32_  = token::intern("u32");
+
+    let path_str : Vec<String> = path.segments.iter().map(
+        |&PathSegment { identifier: Ident { name: ref n, .. }, .. }| n.as_str().to_string()
+    ).collect();
+
+    match path_str.connect("::").as_slice() {
+        "std::string::String" | "String" => Some(StringType),
+        "int" | "i8" | "i16" | "i32" => Some(IntType),
+        "uint" | "u8" | "u16" | "u32" => Some(UintType),
+        _ => None,
+    }
+}
+
+fn model_template(ecx: &mut ExtCtxt, span: Span, meta_item: &MetaItem, item: &Item, push: |P<Item>|) {
+    match meta_item.node {
+        MetaWord(_) => {
+            let pat_attr_type = Path::new(vec!("rtmpl", "AttrType"));
+            let lit_attr_type = Literal(pat_attr_type);
+            let pat_option_attr_type = Path::new_(vec!("std", "option", "Option"), None, vec!(box lit_attr_type), true);
+            let lit_option_attr_type = Literal(pat_option_attr_type);
+
+            let get_type = |cx: &mut ExtCtxt, span: Span, substr: &Substructure| -> P<Expr> {
+                //match attr {
+                //    "world" => Some(StringType),
+                //    _ => None,
+                //}
+                //let none_path = cx.path_global(span, vec!(cx.ident_of("std"), cx.ident_of("option"), cx.ident_of("None")));
+                let struct_def = match item.node {
+                    ItemStruct(ref struct_def, _) => struct_def,
+                    _ => panic!("We only support structures for now."),
+                };
+                let mut fields : Vec<syntax::ast::Arm> = struct_def.fields.iter().filter_map(|&Spanned {node: ref node, span: _}|
+                    match node.kind {
+                        NamedField(ident, _) => match node.ty.node {
+                            TyPath(ref path, _, _) => match path_to_attr_type(path) {
+                                Some(t) => Some( cx.arm(span, vec!(cx.pat_lit(span, cx.expr_str(span, token::get_ident(ident)))), cx.expr_some(span,
+                                        cx.expr_path(cx.path_global(span, vec!(cx.ident_of("rtmpl"), cx.ident_of(format!("{}", t).as_slice())) ))
+                                    )) ),
+                                None => None,
+                            },
+                            _ => None,
+                        },
+                        UnnamedField(_) => panic!("No support for unnamed fields (yet)."),
+                    }
+                ).collect();
+
+                fields.push(cx.arm(span, vec!(cx.pat_wild(span)), cx.expr_none(span)));
+
+                cx.expr_match(span,
+                    substr.nonself_args[0].clone(),
+                    fields,
+                )
+            };
+
+            fn get_str(cx: &mut ExtCtxt, span: Span, substr: &Substructure) -> P<Expr> {
+                cx.expr_lit(span, LitStr(InternedString::new("Test"), CookedStr))
+            }
+
+            fn get_int(cx: &mut ExtCtxt, span: Span, substr: &Substructure) -> P<Expr> {
+                cx.expr_lit(span, LitInt(42, UnsuffixedIntLit(Plus)))
+            }
+
+            fn get_uint(cx: &mut ExtCtxt, span: Span, substr: &Substructure) -> P<Expr> {
+                cx.expr_lit(span, LitInt(43, UnsuffixedIntLit(Plus)))
+            }
+
+            macro_rules! md (
+                ($name:expr, $expl_self:expr, $args:expr, $ret:expr, $f:ident) => { {
+                    //let inline = cx.meta_word(span, InternedString::new("inline"));
+                    let attrs = Vec::new();//vec!(cx.attribute(span, inline));
+                    MethodDef {
+                        name: $name,
+                        generics: LifetimeBounds::empty(),
+                        explicit_self: $expl_self,
+                        args: $args,
+                        ret_ty: $ret,
+                        attributes: attrs,
+                        combine_substructure: combine_substructure(|a, b, c| {
+                            $f(a, b, c)
+                        })
+                    }
+                } }
+                );
+
+            let md_get_type =  md!(
+                "__get_type",
+                None,
+                vec!(
+                    borrowed( box Literal( Path::new(vec!("str")) )),
+                    Literal( Path::new_(vec!("std", "option", "Option"), None, vec!(box Self), true) ),
+                ),
+                lit_option_attr_type,
+                get_type
+            );
+
+            macro_rules! md_get (
+                ($name:expr, $expl_self:expr, $ret:expr, $f:ident) => {
+                    md!(
+                        $name,
+                        $expl_self,
+                        vec!(borrowed( box Literal( Path::new(vec!("str")) ))),
+                        $ret,
+                        $f
+                    )
+                }
+                );
+
+            let trait_def = TraitDef {
+                span: span,
+                attributes: Vec::new(),
+                path: Path::new(vec!("rtmpl", "Model")),
+                additional_bounds: Vec::new(),
+                generics: LifetimeBounds::empty(),
+                methods: vec!(
+                    md_get_type,
+                    md_get!("__get_string", borrowed_explicit_self(), borrowed( box Literal(Path::new(vec!("str"))) ), get_str),
+                    md_get!("__get_int", borrowed_explicit_self(), Literal(Path::new(vec!("int"))), get_int),
+                    md_get!("__get_uint", borrowed_explicit_self(), Literal(Path::new(vec!("uint"))), get_uint)
+                    )
+            };
+            trait_def.expand(ecx, meta_item, item, push)
+        }
+        _ => {
+            ecx.span_err(meta_item.span, "unsupported trait list in `model`");
+        }
+        /*
+        MetaNameValue(_, ref l) => {
+            ecx.span_err(l.span, "unexpected value in `model`");
+        }
+        MetaList(_, ref titems) if titems.len() == 0 => {
+            ecx.span_warn(meta_item.span, "empty trait list in `model`");
+        }
+        MetaList(_, ref titems) => {
+            for titem in titems.iter().rev() {
+                match titem.node {
+                    MetaNameValue(ref tname, _) |
+                        MetaList(ref tname, _) |
+                        MetaWord(ref tname) => {
+                            macro_rules! expand(($func:path) => ($func(ecx, titem.span,
+                                                                       &**titem, item,
+                                                                       |i| push(i))));
+                            match tname.get() {
+                                //"Clone" => expand!(clone::expand_deriving_clone),
+                                //"Hash" => expand!(hash::expand_deriving_hash),
+                                //"Encodable" => expand!(encodable::expand_deriving_encodable),
+                                //"Decodable" => expand!(decodable::expand_deriving_decodable),
+                                //"PartialEq" => expand!(eq::expand_deriving_eq),
+                                //"Eq" => expand!(totaleq::expand_deriving_totaleq),
+                                //"PartialOrd" => expand!(ord::expand_deriving_ord),
+                                //"Ord" => expand!(totalord::expand_deriving_totalord),
+                                //"Rand" => expand!(rand::expand_deriving_rand),
+                                //"Show" => expand!(show::expand_deriving_show),
+                                //"Zero" => expand!(zero::expand_deriving_zero),
+                                //"Default" => expand!(default::expand_deriving_default),
+                                //"FromPrimitive" => expand!(primitive::expand_deriving_from_primitive),
+                                //"Send" => expand!(bounds::expand_deriving_bound),
+                                //"Sync" => expand!(bounds::expand_deriving_bound),
+                                //"Copy" => expand!(bounds::expand_deriving_bound),
+                                ref tname => {
+                                    ecx.span_err(titem.span,
+                                                format!("unknown `model` trait: `{}`",
+                                                        *tname).as_slice());
+                                }
+                            };
+                        }
+                }
+            }
+        }
+        */
+    };
+}
+
+#[plugin_registrar]
+pub fn plugin_registrar(reg: &mut Registry) {
+    reg.register_syntax_extension(token::intern("model"), Decorator(box model_template));
+}
+
---
+++ tests/simple_test.rs
@@ -0,0 +1,83 @@
+#![feature(macro_rules)]
+#![feature(phase)]
+
+#[phase(plugin, link)]
+extern crate rtmpl;
+
+use rtmpl::Model;
+
+trait Template<M> {
+    fn render(model: &M) -> String;
+}
+
+struct TestTemplate<M> {
+    res: String,
+}
+
+#[model]
+struct WorldModel {
+    world: String,
+    min_temp: u32,
+    max_temp: i32,
+}
+
+macro_rules! template_from_file( ($file:expr) => (
+    TestTemplate::from_str(include_str!($file))
+) )
+
+impl <M/*: Model*/> TestTemplate<M> {
+    fn from_str(template: &str) -> TestTemplate<M> {
+        TestTemplate { res: String::from_str(template) }
+    }
+
+    fn render(&self, model: &M) -> &str {
+        self.res.as_slice()
+    }
+}
+
+#[test]
+fn simple_str() {
+    let templ = "hello world";
+
+//    assert!(Template::from_str(templ).render() == "hello world");
+}
+
+#[test]
+fn compound_str() {
+    let templ_str = "Hello {world}!";
+
+    let tmpl = TestTemplate::from_str(templ_str);
+
+    /* GOAL TEMPLATE CODE:
+       struct Template__WorldModel {
+           fn render(&self, model: &M) -> String {
+               format!("Hello {:s}!", model.world);
+           }
+       }
+     */
+
+    let model = WorldModel { world: String::from_str("Earth"), min_temp: 184, max_temp: 330 };
+
+    // XXX: The none::<> stuff is a hack until Uniform Function Call Syntax (UFCS) is implemented,
+    // see http://stackoverflow.com/questions/23674538/name-resolving-error-when-implementing-static-method-from-a-trait
+    assert!(Model::__get_type("world", None::<WorldModel>).unwrap() == rtmpl::StringType);
+    assert!(Model::__get_type("min_temp", None::<WorldModel>).unwrap() == rtmpl::UintType);
+    assert!(Model::__get_type("max_temp", None::<WorldModel>).unwrap() == rtmpl::IntType);
+    assert!(Model::__get_type("n/a", None::<WorldModel>).is_none());
+    assert!(model.__get_string("world") == "Earth");
+    assert!(model.__get_uint("min_temp") == 184);
+    assert!(model.__get_int("max_temp") == 330);
+
+    assert!(tmpl.render(&model) == "Hello Earth!");
+}
+
+#[test]
+fn from_file() {
+    //trace_macros!(true);
+    let template = template_from_file!("hello.tmpl");
+
+    let res = template.render(&1u32);
+
+    assert!(res == "Hello World!\n");
+}
+
